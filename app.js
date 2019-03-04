a6603740482572ea725ee08aacc803c1d5f3bb82
Grauero <grayero6996@gmail.com>
2019-03-04 16:07:08 +0300

    Add signup/signin/signout routes, conected DB to server

--- app.js
+++ app.js
@@ -1,18 +1,44 @@
 /* eslint-disable no-console */
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const mongoose = require('mongoose');
 const passport = require('passport');
 
-const auth = require('./routes/auth');
+require('./models/User');
 require('./config/passport')(passport);
+const auth = require('./routes/auth');
+const keys = require('./config/keys');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+(async function connectToDB() {
+  try {
+    await mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
+  } catch (err) {
+    throw new Error('Connection to DB failed');
+  }
+}());
 
 app.get('/', (req, res) => {
   res.send('Home Page');
 });
 
+app.use(cookieParser());
+app.use(
+  session({
+    secret: keys.secret,
+    resave: false,
+    saveUninitialized: false
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use((req, res, next) => {
+  res.locals.user = req.user || null;
+  next();
+});
 app.use('/auth', auth);
 
 app.listen(PORT, () => console.log(`Server on port ${PORT}`));
--- config/passport.js
+++ config/passport.js
@@ -1,19 +1,51 @@
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
 
+const User = mongoose.model('users');
 const keys = require('./keys');
 
 module.exports = (passport) => {
   passport.use(
     new GoogleStrategy(
       {
-        clientId: keys.googleClientID,
+        clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback',
         userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo',
         proxy: true
       },
-      (accessToken, refreshToken, profile, done) => {}
+      async (accessToken, refreshToken, profile, done) => {
+        let image = profile.photos[0].value;
+        image = image.substring(0, image.indexOf('?')); // remove default size of profile image
+
+        let newUser = {
+          googleID: profile.id,
+          firstName: profile.name.givenName,
+          lastName: profile.name.familyName,
+          email: profile.emails[0].value,
+          image
+        };
+
+        // check for existng user
+        const existingUser = await User.findOne({ googleID: profile.id });
+        if (existingUser) {
+          // return user
+          done(null, existingUser);
+        } else {
+          // create user
+          newUser = await new User(newUser).save();
+          done(null, newUser);
+        }
+      }
     )
   );
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(async (id, done) => {
+    const user = await User.findById(id);
+    done(null, user);
+  });
 };
