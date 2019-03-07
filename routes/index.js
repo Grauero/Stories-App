c9f3af3c906abf97763aecd38e80e12a27a88c36
Grauero <grayero6996@gmail.com>
2019-03-07 17:05:31 +0300

    Implement GET /dashboard route

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,16 @@
 const router = require('express').Router();
+const mongoose = require('mongoose');
 
+const Story = mongoose.model('stories');
 const { ensureAuthenticated, ensureGurest } = require('../utils/auth');
 
 router.get('/', ensureGurest, (req, res) => {
   res.render('index/welcome');
 });
 
-router.get('/dashboard', ensureAuthenticated, (req, res) => {
-  res.render('index/dashboard');
+router.get('/dashboard', ensureAuthenticated, async (req, res) => {
+  const stories = await Story.find({ user: req.user.id });
+  res.render('index/dashboard', { stories });
 });
 
 router.get('/about', (req, res) => {
